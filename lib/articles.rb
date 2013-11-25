ab562aa26c6f3f5fcfaf936f16ce315ce0a32780
brycelambert <brycelambert@gmail.com>
2013-11-25 15:02:33 -0500

    fix twitter records methods

--- lib/articles.rb
+++ lib/articles.rb
@@ -16,7 +16,7 @@ module Monroe
     def name(name)
       first_name = name.split(' ')[0]
       last_name = name.split(' ')[1]
-      url = BASE_URI + "articles?first_name=" + first_name + "&last_name" + last_name + "&api_key=" + api_key
+      url = BASE_URI + "articles?first_name=" + first_name + "&last_name" + last_name + "&api_key=" + Monroe.key
       return JSON.parse(open(url.to_s).read)
     end
     
